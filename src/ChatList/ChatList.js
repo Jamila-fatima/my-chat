021db5e86b0500bc3d4118067776003841b1d784
Jamila-fatima <49222809+Jamila-fatima@users.noreply.github.com>
2019-10-16 08:23:16 -0700

    dashboard
    chatlist and dashboard

---
+++ src/ChatList/ChatList.js
@@ -0,0 +1,32 @@
+  
+import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
+// import List from '@material-ui/core/List';
+// import ListItem from '@material-ui/core/ListItem';
+// import ListItemText from '@material-ui/core/ListItemText';
+// import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+// import Avatar from '@material-ui/core/Avatar';
+// import Typography from '@material-ui/core/Typography';
+import Style from './Style';
+// import Divider from '@material-ui/core/Divider';
+// import Button from '@material-ui/core/Button';
+// import ListItemIcon from '@material-ui/core/ListItemIcon';
+// import NotificationImportant from '@material-ui/icons/NotificationImportant';
+
+
+import  { Component } from 'react'
+
+
+
+
+class ChatList extends Component {
+    render() {
+        return (
+            <div>
+                hello from chat list
+            </div>
+        );
+    }
+}
+
+export default withStyles(Style)(ChatList);
--- src/DashBoard/DashBoard.js
+++ src/DashBoard/DashBoard.js
@@ -1,14 +1,71 @@
-
+import ChatList from '../ChatList/ChatList';
 import React, { Component } from 'react';
+import Styles from './Styles';
+
+
+const firebase = require("firebase");
 
 class DashBoard extends Component {
+    constructor() {
+        super();
+        this.state = {
+            selectedChat: null,
+            newChatFormVisible: false,
+            email: null,
+            chats: []
+        }
+    }
+
+
+
     render() {
         return (
             <div>
-                hello world from dashboard
+
+                <div>hello from dashboard</div>
+                <ChatList history={this.props.history} newChatBtnFn={this.newChatBtnClicked} selectChatFn={this.selectChat}
+                    chats={this.state.chats}
+                    userEmail={this.state.email}
+                    selectedChatIndex={this.state.selectedChat} />
+
             </div>
         );
     }
+
+    selectChat = (chatIndex) => {
+        console.log('selected a chat', chatIndex);
+    }
+
+
+    newChatBtnClicked = () => this.setState({
+        newChatFormVisible: true, selectChat: null
+    });
+
+    componentWillMount = () => {
+        firebase.auth().onAuthStateChanged(async _usr => {
+            if (!_usr)
+                this.props.history.push("/LogIn");
+            else {
+                await firebase
+                    .firestore()
+                    .collection('chats')
+                    .where('user', 'array-contains', _usr.email)
+                    .onSnapshot(async res => {
+                        const chats = res.docs.map(_doc => _doc.data());
+                        await this.setState({
+                            email: _usr.email,
+                            chats: chats
+                        });
+                        console.log(this.state);
+
+                    })
+
+            }
+
+        }
+        )
+    }
+
 }
 
 export default DashBoard;
